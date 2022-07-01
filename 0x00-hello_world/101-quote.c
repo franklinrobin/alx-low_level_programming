3d56743c97fa29af6cb4b0d30faf3cc67e099dbf
Frankline <franklinerobin86@gmail.com>
2022-06-30 17:37:16 -0700

    task 101 commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,24 +3,20 @@
 #include <unistd.h>
 
 /**
+ *     * main - prints exactly "and that piece of art is useful"
  *
- *  * main - prints exactly "and that piece of art is useful"
+ *     * - Gideon Obae, 2021-08-26",
  *
- *   * - Gideon Obae, 2021-08-26",
- *
- *    * followed by a new line, to the standard error.
+ *     * followed by a new line, to the standard error.
  *
  *     * Return: 1 if success.
- *
  */
 
 int main(void)
 {
 
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-		return (1);
+	return (1);
 
 }
-
-
