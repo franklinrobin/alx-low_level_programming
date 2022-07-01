0fc19121785d842c0bb19b72ac67ea979df583b2
Frankline <franklinerobin86@gmail.com>
2022-07-01 16:45:07 -0700

    commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include<studio.h>
+#include<stdlib.h>
+
+/**
+*main - main block
+*Description - get a random number and check it's last digit compare it with 5
+*Return: 0
+*/
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+
+}
