9016ca8d86d49b8ef679367a52cdc07eee643e18
Frankline <franklinerobin86@gmail.com>
2022-07-04 20:54:15 -0700

    task 8 commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,25 +1,22 @@
 #include <stdio.h>
-
 /*
- * main -Entry point
+ * function main - entry point
  * description - printing base 10 numbers
- * Return : 0
-*/
-int main (void)
+ * Return: 0
+ */
+int main(void)
 {
 	int i, y;
+
 	for (i = 0; i < 10; i++)
-	    {
-		    putchar (i + '0');
+	{
+	putchar (i + '0');
+	}
+	for (y = 'a'; y <= 'f'; y++)
+	{
+	putchar (y);
+	}
 
-	    }
-	         for (y = 'a'; y <= 'f'; y++)
-		     {
-			     putchar (y);
-
-		     }
-	       putchar ('\n');
-	       return (0);
-			            
-	
+	putchar ('\n');
+	return (0);
 }
