f2e6c3c884d4e7a851c2d53ef2624d2366f30cce
Frankline <franklinerobin86@gmail.com>
2022-07-05 16:12:07 -0700

    task 3 commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,38 @@
+nclude "main.h"
+
+/**
+ * _islower - Short description, single line
+ * @c: contains value to be compared
+ * Description: Short description
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+
+
+
+{
+
+
+
+if (c > 'a' && c < 'z')
+
+{
+
+return (1);
+
+}
+
+
+
+else
+
+{
+
+return (0);
+
+}
+
+
+
+}
