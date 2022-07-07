19b58685be79d993716a603baadf6610a0eee102
Frankline <franklinerobin86@gmail.com>
2022-07-07 03:29:44 -0700

    task 0 commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+ * main - Debugging.
+ * Description - main head
+ * Return: 0
+ */
+
+int main(void)
+
+{
+
+	int i;
+
+
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+
+}
