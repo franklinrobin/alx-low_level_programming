380c6939cd9354a1042377d47ab4263554117552
Frankline <franklinerobin86@gmail.com>
2022-07-07 14:43:02 -0700

    task 101 commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,34 +1,50 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer
- * @n:.input integer parameter
+ * print_number - Print integer
+ * @n: number
  */
 
 void print_number(int n)
 
 {
 
-unsigned int i = n;
+	unsigned int n2, number, base10 = 1;
 
-if (n < 0)
+	if (n < 0)
 
-{
+	{
+		_putchar('-');
 
-_putchar(45);
+		n2 = -n;
+	}
 
-i = -i;
+	else
+	{
+		n2 = n;
 
-}
+	}
+	number = n2;
+	/* Obtenemos su base */
+	while (number > 9)
+	{
+		number = number / 10;
 
-if (i / 10)
+		base10 = base10 * 10;
+	}
 
-{
+	/* Descomponemos en base 10*/
+	number = n2;
+	while (base10 > 1)
 
-print_number(i / 10);
+	{
+		_putchar((number / base10) + '0');
 
-}
+		number = number % base10;
 
-_putchar(i % 10 + '0');
+		base10 = base10 / 10;
+	}
+
+	_putchar((n2 % 10) + '0');
 
 }
