6bcc2e1887afde0660c301b13f7b82d018402163
Frankline <franklinerobin86@gmail.com>
2022-08-24 07:26:21 -0700

    Hello World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,22 +1,14 @@
 #include <stdio.h>
-
-#include <unistd.h>
-
 /**
- *     * main - prints exactly "and that piece of art is useful"
+ * main - Entry point
  *
- *     * - Gideon Obae, 2021-08-26",
+ * Description: Program that prints to the standard error
  *
- *     * followed by a new line, to the standard error.
- *
- *     * Return: 1 if success.
+ * Return: 1 (Success)
  */
-
 int main(void)
 {
-
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
-
 }
